7954fe739a0bd45cfaa71610965d3f7f31e52e35
BijakLaut <bijaklaoet@gmail.com>
2023-12-27 16:04:32 +0700

    Adding loading to main content

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -3,6 +3,9 @@ import "../globals.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Sidebar } from "../../../components/Sidebar/Sidebar";
+import { Suspense } from "react";
+import SidebarLoading from "../../../components/Loading/SidebarLoading";
+import Loading from "./loading";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,12 +20,15 @@ export default function AdminLayout({
   return (
     <section className="flex flex-row">
       <Sidebar />
-      <main
-        data-theme={"dracula"}
-        className="ms-[17rem] min-h-screen w-full p-10"
-      >
-        {children}
-      </main>
+
+      <Suspense fallback={<Loading />}>
+        <main
+          data-theme={"dracula"}
+          className="ms-[17rem] min-h-screen w-full p-10"
+        >
+          {children}
+        </main>
+      </Suspense>
       <ToastContainer enableMultiContainer containerId={"Main"} theme="dark" />
     </section>
   );
---
+++ src/app/(admin)/loading.tsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-y-3">
+      <span className="loading loading-spinner loading-lg"></span>
+      <p className="text-lg font-semibold">Loading</p>
+    </div>
+  );
+};
+
+export default Loading;
