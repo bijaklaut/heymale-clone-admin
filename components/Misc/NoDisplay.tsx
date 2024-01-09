caa26aca5816ac136d44e0556644ad5659c8417e
Hudaa Eka Saputra <hudaaputra72@gmail.com>
2024-01-09 21:28:25 +0700

    Adding no display component

---
+++ components/Misc/NoDisplay.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { NoDisplaySvg } from "./SvgGroup";
+
+interface ThisProps {
+  text: string;
+}
+
+const NoDisplay = ({ text }: ThisProps) => {
+  return (
+    <div className="mx-auto mt-10 flex w-max flex-col items-center justify-center rounded-md bg-white px-16 py-10 text-neutral">
+      <NoDisplaySvg className="h-10 w-10 stroke-current" />
+      <p className="mt-3">{text}</p>
+    </div>
+  );
+};
+
+export default NoDisplay;
