f120b56af724ed34dceb0730891364e802e1c6a5
Hudaa Eka Saputra <hudaaputra72@gmail.com>
2024-02-06 10:08:47 +0700

    Category select components

---
+++ components/Modals/Voucher/CategoryDisplay.tsx
@@ -0,0 +1,28 @@
+import { CategoryTypes } from "../../../services/types";
+import { MouseEventHandler } from "react";
+import { CrossSvg } from "../../Misc/SvgGroup";
+
+interface ThisProps {
+  key: number | string;
+  category: CategoryTypes;
+  deselect: MouseEventHandler<HTMLButtonElement>;
+}
+
+const CategoryDisplay = ({ key, category, deselect }: ThisProps) => {
+  return (
+    <div
+      key={key}
+      className="relative flex items-center gap-x-3 rounded-md bg-white px-3 py-2 shadow-lg"
+    >
+      <span className="text-sm text-neutral">{category.name}</span>
+      <button
+        className="absolute right-1 rounded-md text-neutral/50 transition-all hover:bg-slate-300/50 hover:text-neutral/70"
+        onClick={deselect}
+      >
+        <CrossSvg className="w-7 fill-current" />
+      </button>
+    </div>
+  );
+};
+
+export default CategoryDisplay;
---
+++ components/Modals/Voucher/CategoryOption.tsx
@@ -0,0 +1,30 @@
+import { ChangeEventHandler, InputHTMLAttributes } from "react";
+import { CategoryTypes } from "../../../services/types";
+import { CircleCheckSvg } from "../../Misc/SvgGroup";
+
+interface ThisProps extends InputHTMLAttributes<HTMLInputElement> {
+  key: number | string;
+  category: CategoryTypes;
+  selectHandler: ChangeEventHandler<HTMLInputElement>;
+}
+
+const CategoryOption = ({ key, category, selectHandler }: ThisProps) => {
+  return (
+    <li key={key} id={category._id}>
+      <label className="label w-full cursor-pointer rounded-md p-2 transition-all">
+        <input
+          type="checkbox"
+          className="peer hidden"
+          value={category._id}
+          onChange={selectHandler}
+        />
+        <div className="w-full">
+          <span>{category.name}</span>
+        </div>
+        <CircleCheckSvg className="w-7 scale-0 fill-current transition-all peer-checked:scale-100" />
+      </label>
+    </li>
+  );
+};
+
+export default CategoryOption;
