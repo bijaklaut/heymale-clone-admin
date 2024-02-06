73c21cb0e73a8e8e52320aa68fd95e8ae1c2b048
Hudaa Eka Saputra <hudaaputra72@gmail.com>
2024-02-06 21:12:12 +0700

    Adding option dot svg

--- components/Misc/SvgGroup.tsx
+++ components/Misc/SvgGroup.tsx
@@ -727,6 +727,31 @@ export const CrossSvg = (props: { className?: string }) => {
   );
 };
 
+export const OptionDotSvg = (props: { className?: string }) => {
+  const { className } = props;
+  return (
+    <svg
+      className={className}
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      transform="rotate(90)"
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        <path d="M12,10a2,2,0,1,1-2,2A2,2,0,0,1,12,10ZM4,14a2,2,0,1,0-2-2A2,2,0,0,0,4,14Zm16-4a2,2,0,1,0,2,2A2,2,0,0,0,20,10Z"></path>
+      </g>
+    </svg>
+  );
+};
+
 export const Template = (props: { className?: string }) => {
   const { className } = props;
   return (
