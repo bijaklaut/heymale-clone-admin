df546ec49553102b20e6000e39a94d23a48eab95
Hudaa Eka Saputra <hudaaputra72@gmail.com>
2024-03-03 22:45:43 +0700

    implementing constants variable, deleting auth sign out

--- services/actions.ts
+++ services/actions.ts
@@ -5,54 +5,47 @@ import { cookies } from "next/headers";
 import { jwtDecode } from "jwt-decode";
 import { GetCourierRatesTypes, UserToken } from "./types";
 import axios from "axios";
-
-const BITESHIP = process.env.BITESHIP_BASEURL;
-const BTS_APIKEY = process.env.BITESHIP_APIKEY;
-
-export async function authSignout() {
-  cookies().delete("token");
-  redirect("/signin");
-}
+import { BITESHIP_APIKEY, BITESHIP_BASEURL } from "../constants";
 
 export const redirectRoot = async () => redirect("/");
 
 export async function getUserToken() {
   const cookieStore = cookies();
-  const token = cookieStore.get("token")?.value;
+  const token = cookieStore.get("accessToken")?.value;
   const jwt = Buffer.from(token!, "base64").toString("ascii");
   const { id } = jwtDecode<UserToken>(jwt);
   return id;
 }
 
 export const getArea = async (input: string) => {
-  const url = `${BITESHIP}/v1/maps/areas?countries=ID&input=${input}&type=single`;
+  const url = `${BITESHIP_BASEURL}/v1/maps/areas?countries=ID&input=${input}&type=single`;
   const response = await axios({
     url,
     method: "GET",
-    headers: { Authorization: `Bearer ${BTS_APIKEY}` },
+    headers: { Authorization: `Bearer ${BITESHIP_APIKEY}` },
   });
 
   return response.data;
 };
 
 export const getCourierRates = async (data: GetCourierRatesTypes) => {
-  const url = `${BITESHIP}/v1/rates/couriers`;
+  const url = `${BITESHIP_BASEURL}/v1/rates/couriers`;
   const response = await axios({
     url,
     method: "POST",
     data,
-    headers: { Authorization: `Bearer ${BTS_APIKEY}` },
+    headers: { Authorization: `Bearer ${BITESHIP_APIKEY}` },
   });
 
   return response.data;
 };
 
 export const trackOrder = async (id: string) => {
-  const url = `${BITESHIP}/v1/trackings/${id}`;
+  const url = `${BITESHIP_BASEURL}/v1/trackings/${id}`;
   const response = await axios({
     url,
     method: "GET",
-    headers: { Authorization: `Bearer ${BTS_APIKEY}` },
+    headers: { Authorization: `Bearer ${BITESHIP_APIKEY}` },
   });
 
   return response.data;
