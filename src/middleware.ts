2c369ebc480ea0a59a3a1535108899ab40359fff
Hudaa Eka Saputra <hudaaputra72@gmail.com>
2024-03-03 22:53:02 +0700

    implementing access and refresh token new schema

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,43 +1,123 @@
 "use server";
 
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest } from "next/server";
+import { API_BASEURL, JWT_SECRET } from "../constants";
+import { jwtVerify } from "jose";
 
 // This function can be marked `async` if using `await` inside
-export default function middleware(request: NextRequest) {
-  const paths = [
-    "/category",
-    "/payment",
-    "/product",
-    "/user",
-    "/transaction",
-    "/history",
-    "/voucher",
-  ];
+export default async function middleware(request: NextRequest) {
+  const url = request.nextUrl.clone();
 
   for (const path of paths) {
     if (
       request.nextUrl.pathname.startsWith(path) ||
       request.nextUrl.pathname.endsWith("/")
     ) {
-      let token = request.cookies.get("token")?.value;
-      if (token) {
-        return NextResponse.next();
-      } else {
-        const url = request.nextUrl.clone();
+      let refreshToken = request.cookies.get("refreshToken")?.value;
+      let accessToken = request.cookies.get("accessToken")?.value;
+
+      if (!refreshToken || (!refreshToken && !accessToken)) {
         url.pathname = "/signin";
-        return NextResponse.redirect(url);
+        const response = NextResponse.redirect(url);
+        response.cookies.delete("refreshToken");
+        response.cookies.delete("accessToken");
+
+        return response;
+      }
+
+      if (refreshToken && !accessToken) {
+        const decodedToken = Buffer.from(refreshToken, "base64").toString(
+          "ascii",
+        );
+        const headers = new Headers({ Cookie: `refreshToken=${decodedToken}` });
+        const result = await fetch(`${API_BASEURL}/api/v1/user/refresh`, {
+          credentials: "include",
+          method: "POST",
+          headers,
+        });
+
+        const getCookie = result.headers.get("Set-Cookie");
+        let cleanedCookies: any[] = [];
+
+        if (!getCookie) {
+          url.pathname = "/signin";
+          return NextResponse.redirect(url);
+        }
+
+        let splitCookies = getCookie?.split("HttpOnly, ");
+
+        splitCookies.forEach((cookie) => {
+          let transformed = cookie.split("; ");
+          cleanedCookies.push(transformed);
+        });
+
+        cleanedCookies.forEach((baked: string, index) => {
+          cleanedCookies[index] = baked[0].split("=")[1];
+        });
+
+        const [newAccessToken] = cleanedCookies;
+
+        const accessEncoded = Buffer.from(newAccessToken, "ascii").toString(
+          "base64",
+        );
+
+        const response = NextResponse.next();
+
+        response.cookies.set({
+          name: "accessToken",
+          value: accessEncoded,
+          maxAge: 60 * 1,
+          httpOnly: true,
+        });
+
+        return response;
+      }
+
+      if (accessToken) {
+        try {
+          const decodedToken = Buffer.from(accessToken, "base64").toString(
+            "ascii",
+          );
+
+          await jwtVerify(decodedToken, new TextEncoder().encode(JWT_SECRET));
+          return NextResponse.next();
+        } catch (error: any) {
+          url.pathname = "/signin";
+          return NextResponse.redirect(url);
+        }
       }
     }
   }
 
   if (request.nextUrl.pathname.startsWith("/signin")) {
-    let token = request.cookies.get("token")?.value;
+    let token = request.cookies.get("accessToken")?.value;
 
     if (token) {
-      const url = request.nextUrl.clone();
-      url.pathname = "/";
-      return NextResponse.redirect(url);
+      try {
+        const decodedToken = Buffer.from(token, "base64").toString("ascii");
+        const url = request.nextUrl.clone();
+        url.pathname = "/";
+
+        await jwtVerify(decodedToken, new TextEncoder().encode(JWT_SECRET));
+        return NextResponse.redirect(url);
+      } catch (error: any) {
+        request.cookies.delete("accessToken");
+
+        return NextResponse.next();
+      }
     }
   }
 }
+
+const paths = [
+  "/category",
+  "/payment",
+  "/product",
+  "/user",
+  "/transaction",
+  "/history",
+  "/voucher",
+  "/order",
+  "/shipment",
+];
