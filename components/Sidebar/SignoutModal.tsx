a982b2701a218394f138b015327c76ddf3c97ec6
Hudaa Eka Saputra <hudaaputra72@gmail.com>
2024-03-03 22:54:42 +0700

    applying new sign out schema

--- components/Sidebar/SignoutModal.tsx
+++ components/Sidebar/SignoutModal.tsx
@@ -1,22 +1,27 @@
 "use client";
 
 import { toast } from "react-toastify";
-import { useCallback, useEffect, useState } from "react";
-import { authSignout } from "../../services/actions";
+import { useCallback, useState } from "react";
 import { simpleModalHandler } from "../../services/helper";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 export const SignoutModal = () => {
+  const router = useRouter();
   const [loading, setLoading] = useState(false);
 
   const signoutHandler = useCallback(async () => {
     try {
       setLoading(true);
-      await authSignout();
-
+      const { data } = await axios({
+        url: "/api/signout",
+        method: "POST",
+      });
       setTimeout(() => {
         setLoading(false);
         simpleModalHandler("logout", false);
-        toast.success("Logged out", { containerId: "Signin" });
+        toast.success(data.message, { containerId: "Main" });
+        router.push("/signin");
       }, 700);
     } catch (error: any) {
       setTimeout(() => {
@@ -34,7 +39,7 @@ export const SignoutModal = () => {
         className="w-full text-left"
         onClick={() => simpleModalHandler("logout", true)}
       >
-        Log out
+        Sign out
       </button>
       <dialog
         data-theme={"skies"}
@@ -43,17 +48,17 @@ export const SignoutModal = () => {
       >
         <div className="modal-box absolute flex flex-col items-center px-5 py-8">
           <h3 className="mb-3 text-center text-base font-semibold text-white">
-            Are you sure to log out?
+            Are you sure to sign out?
           </h3>
           <div className="modal-action flex">
             {!loading ? (
               <button className="btn btn-error btn-sm" onClick={signoutHandler}>
-                Log out
+                Sign out
               </button>
             ) : (
               <button className="btn btn-sm">
                 <span className="loading loading-spinner loading-sm"></span>
-                Logging out..
+                Signing out..
               </button>
             )}
             <form method="dialog">
