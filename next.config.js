b320bd3314dc5a1debdc3d4de09d282054c563c9
Hudaa Eka Saputra <hudaaputra72@gmail.com>
2024-03-06 16:04:15 +0700

    Adding cloud hostname

--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ const nextConfig = {
       {
         hostname: "sore-lime-rabbit-tutu.cyclic.app",
       },
+      {
+        hostname:
+          "cyclic-sore-lime-rabbit-tutu-us-west-1.s3.us-west-1.amazonaws.com",
+      },
     ],
   },
 };
