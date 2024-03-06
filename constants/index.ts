6115ef823069c0ec8a94024bbc3adb1b80fd3eba
Hudaa Eka Saputra <hudaaputra72@gmail.com>
2024-03-06 16:05:54 +0700

    adding constant variables

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,6 @@
 // CLIENT
-export const PUBLIC_API_BASEURL = process.env.NEXT_PUBLIC_API_BASEURL;
-export const PUBLIC_API_IMG = process.env.NEXT_PUBLIC_API_IMG;
+export const PUBLIC_API_BASEURL = process.env.NEXT_PUBLIC_API_BASEURL_LOCAL;
+export const PUBLIC_API_IMG = process.env.NEXT_PUBLIC_API_IMG_LOCAL;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const BITESHIP_BASEURL = process.env.BITESHIP_BASEURL;
 export const BITESHIP_APIKEY = process.env.BITESHIP_APIKEY;
@@ -9,6 +9,6 @@ export const EXPIRED_ACCESS = 60 * 15; // 15 MINUTES
 export const EXPIRED_REFRESH = 60 * 60 * 24 * 14; // 14 DAYS
 
 // SERVER
-export const API_BASEURL = process.env.API_BASEURL;
-export const API_IMG = process.env.API_IMG;
+export const API_BASEURL = process.env.API_BASEURL_LOCAL;
+export const API_IMG = process.env.API_IMG_LOCAL;
 export const API_VER = process.env.API_VER;
